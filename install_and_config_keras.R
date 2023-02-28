01d3085d730727e698ac352fcac537093e75b1dd
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2023-02-28 09:39:51 -0300

    Update install_and_config_keras.R

--- install_and_config_keras.R
+++ install_and_config_keras.R
@@ -1,4 +1,17 @@
 install.packages("keras")
+install.packages("tensorflow")
+
 library(keras)
 library(reticulate)
+library(tensorflow)
+
+path_to_python <- install_python()
+virtualenv_create("r-reticulate", python = path_to_python)
+
+install_tensorflow(envname = "r-reticulate")
+install_keras(envname = "r-reticulate")
+
+library(tensorflow)
+tf$constant("Hello Tensorflow!")
+
 install_keras()

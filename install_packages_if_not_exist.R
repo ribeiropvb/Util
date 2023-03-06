fb717955c973dff9fbfb8a4c50afe2fbe5910f4a
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2023-03-06 18:46:37 -0300

    Update install_packages_if_not_exist.R

--- install_packages_if_not_exist.R
+++ install_packages_if_not_exist.R
@@ -1,4 +1,4 @@
 install_packages_if_not_exist <- function(list.of.packages){
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages)
+  if(length(new.packages)) install.packages(new.packages, quiet = T)
 }

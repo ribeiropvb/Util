02c7b303ad3ba9fe212f99699185929b16890fd1
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2025-01-19 15:39:24 -0300

    Update location_to_wd.R

--- location_to_wd.R
+++ location_to_wd.R
@@ -1,2 +1,3 @@
+# Created by: https://github.com/hbrpaulo
 path_one <- stringr::str_split(rstudioapi::getActiveDocumentContext()$path, '/', simplify = TRUE)
 setwd(paste0(path_one[-length(path_one)], collapse = '/'))
